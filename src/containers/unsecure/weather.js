86fcd278b47067a1cd8908752197ba52c6338799
OliveIT <olivedev92@gmail.com>
2019-03-17 06:31:41 +1000

    Implement weather animation

--- src/containers/unsecure/weather.js
+++ src/containers/unsecure/weather.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ScrollView, View, Image, ImageBackground, Text, Dimensions, TextInput, TouchableOpacity } from 'react-native';
+import { ScrollView, View, Image, ImageBackground, Text, Dimensions, Easing, Animated, TouchableOpacity } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { connect } from "react-redux";
 import { CheckBox } from 'react-native-elements'
@@ -10,13 +10,34 @@ import WeatherBox from '../../components/weatherbox';
 import styles from '../../styles';
 import agreement from '../../../images/agreement.png';
 
+const {width} = Dimensions.get("screen");
 class Weather extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      isTerms: false
+      isTerms: false,
+      offset: 0,
+      max: width * 0.28
     }
+  }
+
+  onPressSubTitle() {
+    let {offset} = this.state;
+    let direction = 1;
+    if (offset >= 0)  direction = -1;
+    const handle = setInterval(() => {
+      offset += 25 * direction;
+      this.setState({
+        offset: offset
+      });
+
+      if (offset <= - this.state.max || offset >= 0) {
+        offset = 0;
+        clearInterval(handle);
+        return;
+      }
+    }, 50);
   }
 
   onCheckTerms() {
@@ -36,22 +57,33 @@ class Weather extends React.Component {
     const texts = text.split('<br />');
     const {width, height} = Dimensions.get("screen");
 
+    const {offset} = this.state;
+
     return (
       <ScrollView style={styles.fullSize}>
-        <ImageBackground source={{uri: presentation.medias [0].url}} style={styles.Weather.headerImage}>
-          <LinearGradient colors={['#00000000', '#00000000', '#000000ff']} style={styles.Weather.headerImage}>
-            <BackButton style={styles.backBtn} navigation={this.props.navigation}/>
+        <ImageBackground source={{uri: presentation.medias [0].url}}
+          style={[styles.Weather.headerImage]}>
+          <LinearGradient colors={['#00000000', '#00000000', '#000000ff']}
+            style={[styles.Weather.headerImage, {marginTop: offset}]}>
+            
+            <View style={{marginTop: -offset}}>
+              <BackButton style={styles.backBtn} navigation={this.props.navigation}/>
+            </View>
+
             <View style={styles.Weather.topContentContainer}>
-              <WeatherBox style={styles.Weather.weather} min={weather.from} max={weather.to} source={{uri: weather.medias [0].url}}/>
-              <Text style={styles.Weather.minorHeaderText}>{name}</Text>
-              <Text style={styles.Weather.mainHeaderText}>{shortName}</Text>
+              <View style={{marginTop: offset}}>
+                <WeatherBox style={[styles.Weather.weather, {opacity: 1 + offset / this.state.max }]}
+                  min={weather.from} max={weather.to} source={{uri: weather.medias [0].url}}/>
+                <Text style={styles.Weather.minorHeaderText}>{name}</Text>
+                <Text style={styles.Weather.mainHeaderText}>{shortName}</Text>
+              </View>
             </View>
           </LinearGradient>
         </ImageBackground>
 
-        <View style={[styles.Weather.mainContainer, styles.borderTopRadius]}>
+        <View style={[styles.Weather.mainContainer, styles.borderTopRadius, {marginTop: offset}]}>
           <Text style={styles.Weather.contentHeader}>{title}</Text>
-          <Text style={styles.Weather.contentSubTitle}>{subTitle}</Text>
+          <Text style={styles.Weather.contentSubTitle} onPress={this.onPressSubTitle.bind(this)}>{subTitle}</Text>
           {texts.map((text, index) => 
             <Text style={styles.Weather.contentText} key={index}>{text}</Text>
           )}
